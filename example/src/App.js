a7fc2d60bdcbb40b3065cd87f01d5100acfeb7f1
lng <liangyu.cs07g@nctu.edu.tw>
2022-11-02 16:40:25 +0800

    -- finish exercise 1.11 -- files need to be uncommited or deleted: example/public/, unicafe/.gitignore

--- example/src/App.js
+++ example/src/App.js
@@ -1,22 +1,41 @@
+import { click } from "@testing-library/user-event/dist/click"
 import { useState } from "react"
 
-const Display = ({ counter }) => <div>{counter}</div>
+const History = (props) => {
+  console.log('props value is ', props)
+  if (props.allClicks.length === 0) {
+    return (
+      <div>the app is used by pressing the buttons</div>
+    )
+  }
+  return (
+    <div>button press history: {props.allClicks.join(' ')}</div>
+  )
+}
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Button = ({ handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
 const App = () => {
-  const [ counter, setCounter] = useState(0)
-
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <>
-      <Display counter={counter}/>
-      <Button onClick={increaseByOne} text='plus'/>
-      <Button onClick={setToZero} text='zero'/>
-      <Button onClick={decreaseByOne} text='minus'/>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </>
   )
 }
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,68 @@
+import { useState } from 'react'
+
+const Button = ({ handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all * 100
+
+  if (all === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>good</td>
+          <td>{good}</td>
+        </tr>
+        <tr>
+          <td>neutral</td>
+          <td>{neutral}</td>
+        </tr>
+        <tr>
+          <td>bad</td>
+          <td>{bad}</td>
+        </tr>
+        <tr>
+          <td>all</td>
+          <td>{all}</td>
+        </tr>
+        <tr>
+          <td>average</td>
+          <td>{average}</td>
+        </tr>
+        <tr>
+          <td>positive</td>
+          <td>{positive} %</td>
+        </tr>
+        </tbody>
+    </table>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <h2>give feedback</h2>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+
+      <h2>statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </>
+  )
+}
+
+export default App
